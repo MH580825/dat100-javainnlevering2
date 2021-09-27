dd86f42c330e3296818ccc8632ee14580a1695b2
mxlars <mxlars300@gmail.com>
2021-09-27 16:36:39 +0200

    Ferdig med alle oppgavene i oblig2. JUnit virker 100% :)

--- src/no/hvl/dat100/lab6/matriser/Matriser.java
+++ src/no/hvl/dat100/lab6/matriser/Matriser.java
@@ -1,27 +1,32 @@
 package no.hvl.dat100.lab6.matriser;
 
 public class Matriser {
-	// vi har også her valgt å legge til main metode og kalle alle funksjonene selv, --
+	// vi har også her valgt å legge til main metode og kalle alle funksjonene selv --
 	// for å få en bedre forståelse og bedre læring :)
 	public static void main(String[] args) {
 		int[][] matrise1 = { { 2, 1, 4 }, { 4, 2, 1 }, { 2, 2, 2 } };
 		int[][] matrise2 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+		System.out.println("Oppgave a)");
 		skrivUt(matrise1); // kaller metoden som skriver ut matrise
 		skrivUt(matrise2); // kaller metoden som skriver ut matrise
+		System.out.println("Oppgave b)");
 		System.out.println(tilStreng(matrise1)); // skriver ut matrise som streng
+		System.out.println("Oppgave c)");
 		int[][] nyMatrise = skaler(5, matrise1); //
 		skrivUt(nyMatrise);
-		
-		boolean lik = erLik(matrise1, matrise2);	//kaller metoden som sjekker om matrisene er like
-		if ( lik == true) {	
-			System.out.println("Matrisene er like");//dersom like, skrives dette ut
-		}
-		else {
-			System.out.println("Matrisene er ulike"); //ellers skrives dette ut
-		}
-	}
+		System.out.println("Oppgave d)");
 
-
+		boolean lik = erLik(matrise1, matrise2); // kaller metoden som sjekker om matrisene er like
+		if (lik == true) {
+			System.out.println("Matrisene er like");// dersom like, skrives dette ut
+		} else {
+			System.out.println("Matrisene er ulike"); // ellers skrives dette ut
+		}
+		System.out.println("Oppgave e)");
+		skrivUt(speile(matrise1)); // kaller metoden for å speile matrisen og skrive ut
+		System.out.println("Oppgave f)");
+		skrivUt(multipliser(matrise1,matrise2));
+}
 	// a)
 	// Metode for å skrive ut matrise
 	public static void skrivUt(int[][] matrise) {
@@ -49,7 +54,8 @@ public class Matriser {
 	}
 
 	// c)
-	// metode som returnerer en ny matrise der alle tall i matrisen er multiplisert med tall
+	// metode som returnerer en ny matrise der alle tall i matrisen er multiplisert
+	// med tall
 	public static int[][] skaler(int tall, int[][] matrise) {
 
 		int[][] nyMatrise = new int[matrise.length][matrise[0].length];
@@ -62,7 +68,7 @@ public class Matriser {
 	}
 
 	// d)
-	//Metode som sjekker om matrisene er like eller ulike
+	// Metode som sjekker om matrisene er like eller ulike
 	public static boolean erLik(int[][] a, int[][] b) {
 		boolean lik = false;
 
@@ -84,15 +90,45 @@ public class Matriser {
 		lik = true;
 		return lik;
 	}
-}
 
 // e)
-//	public static int[][] speile(int[][] matrise) {
-//
-//	}
-//
-//	// f)
-//	public static int[][] multipliser(int[][] a, int[][] b) {
-//
-//	}
-//}
+	// metode for å speile en matrise
+	public static int[][] speile(int[][] matrise) {
+		int[][] speiletMat = new int[matrise.length][matrise[0].length];
+
+		for (int i = 0; i < matrise.length; i++) {
+
+			for (int j = 0; j < matrise[i].length; j++) {
+
+				speiletMat[j][i] = matrise[i][j];
+			}
+		}
+		return speiletMat;
+	}
+
+	// f)
+	// metode for å multiplisere matriser
+	public static int[][] multipliser(int[][] a, int[][] b) {
+		int[][] nyMatrise = new int[a.length][b[0].length];
+
+		if (a.length == b[0].length) {
+
+			for (int row = 0; row < nyMatrise.length; row++) {
+
+				for (int col = 0; col < nyMatrise.length; col++) {
+
+					int sum = 0;
+
+					for (int colb = 0; colb < nyMatrise.length; colb++) {
+
+						sum += a[row][colb] * b[colb][col];
+					}
+					nyMatrise[row][col] += sum;
+				}
+			}
+		} else {
+		}
+
+		return nyMatrise;
+	}
+}
